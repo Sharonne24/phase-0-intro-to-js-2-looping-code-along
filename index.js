a64494c98472c59a734f7c06280f9069e3887be8
Sharonne24 <sawuor395@gmail.com>
2023-03-21 10:28:25 +0300

    Completed assignmwnt

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+// Code your solutions in here.
+
+  function writeCards(names, event) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+
+  const names = ['Guadalupe', 'Ollie', 'Aki'];
+const event = 'birthday';
+
+const messages = writeCards(names,);
+console.log(messages);
+[
+    "Thank you, Guadalupe, for the wonderful surprise gift!",
+    "Thank you, Ollie, for the wonderful surprise gift!",
+    "Thank you, Aki, for the wonderful surprise gift!"
+ ]
+
+ 
+ function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+
